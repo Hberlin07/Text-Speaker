5cea4841417823ce8fc200bd49cb813d7912f7d8
Hberlin07 <155764053+Hberlin07@users.noreply.github.com>
2024-01-05 23:06:04 +0530

    Delete text-speech directory

--- text-speech/speech.js
+++
@@ -1,7 +0,0 @@
-const speak = document.getElementById("speakButton")
-speak.addEventListener("click",()=>{
-    const textToRead = document.getElementById('textToRead').value;
-    const utterance = new SpeechSynthesisUtterance(textToRead);
-    window.speechSynthesis.speak(utterance);
-  }) 
-
